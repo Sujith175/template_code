2beb27783ee5a374a7d196326b5446b3a12bf7f3
Sujith Kumar R <sujithkumar.sk175@gmail.com>
2024-10-25 13:47:20 +0530

    feat: global

---
+++ Express/1. Query params/products.js
@@ -0,0 +1,29 @@
+const Item = [
+  {
+    id: 1,
+    name: "Men's Round Neck Regular Fit T-Shirt ",
+    image:
+      "https://m.media-amazon.com/images/I/610U9GH5gcL._AC_UL480_FMwebp_QL65_.jpg",
+    price: "200",
+    description: "Men's regular fit t-shirt in a vibrant Royal Blue color",
+  },
+  {
+    id: 2,
+    name: "Boys & Girls Coordinate Set, Tshirt Shorts Set ",
+    image:
+      "https://m.media-amazon.com/images/I/51FQl0eyhUL._AC_UL480_FMwebp_QL65_.jpg",
+    price: "800",
+    description:
+      "Durability is a top priority for us, and our Top & Lower Set is built to withstand the active lifestyles of kids",
+  },
+  {
+    id: 3,
+    name: "Women Co Ord Set",
+    image:
+      "https://m.media-amazon.com/images/I/61rOLXOQ6QL._AC_UL480_FMwebp_QL65_.jpg",
+    price: "1200",
+    description:
+      "This Printed Co ord Set Is the Perfect Present for Every Lady in Your House Be It Your Mom , Wife , Sister or Even a Friend",
+  },
+];
+module.exports = Item;
